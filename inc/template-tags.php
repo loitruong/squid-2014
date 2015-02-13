601eecfb91b11c420fd1a5c72b036aed1e4f5287
Joseph Fusco <hello@josephfus.co>
2015-02-13 10:12:12 -0500

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -179,9 +179,7 @@ if ( ! function_exists( 'squid_posted_on' ) ) :
  */
 function squid_posted_on() {
 	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) )
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-
+	
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
